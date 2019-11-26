49316c39fbf9c1fde072e1e39a772d5addc363bb
Chimba <57981107+chimbeev@users.noreply.github.com>
2019-11-26 16:07:47 +0800

    Add files via upload

--- Credit.java
+++ Credit.java
@@ -1,25 +1,34 @@
-//обьявим класс кредит
-//Кредитный калькулятор контроллер. Отвечает за прием данных, анализ, передачу другим
+
+
 import java.util.*;
+import java.util.Date;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-public class Credit
-    {
-       int id_client;
-       int size;
-       int percent;
-       int first_pay_size;
-       int typeOfPayment;
-       int term;
-       Date termOfFirstPayment;
-      public static void main (String[] args) throws IOException
+
+
+
+public class Credit {
+    int id_client;
+    int size;
+    int percent;
+    int first_pay_size;
+    char typeOfPayment;
+    int term;
+    Date termOfFirstPayment;
+
+    // Date termOfFirstPayment;
+    public static void main(String[] args) throws IOException, ParseException
        {
-           File input = new File("input.csv");
+           File input = new File("input3.csv");
            Scanner sc = new Scanner(input);
-           String StrIn = sc.nextLine(); //считаем всю строку с данным кредита
+           String StrIn = sc.nextLine();
+           StrIn = sc.nextLine(); //считаем всю строку с данным кредита
            sc.close();
            Credit NewCred = new Credit(); 
            String [] StrInArray = StrIn.split(";");
@@ -27,9 +36,35 @@ public class Credit
            NewCred.size = Integer.parseInt(StrInArray[1]);
            NewCred.percent = Integer.parseInt(StrInArray[2]);
            NewCred.first_pay_size = Integer.parseInt(StrInArray[3]);
-           NewCred.typeOfPayment= Integer.parseInt(StrInArray[4]);
+           NewCred.typeOfPayment= StrInArray[4].charAt(0);
            NewCred.term = Integer.parseInt(StrInArray[5]);
-           NewCred.termOfFirstPayment = Integer.parseInt(StrInArray[6]);
+           /*Date DATE = new SimpleDateFormat("dd-MM-yyyy").parse(StrInArray[6]);
+           SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
+           String date = DATE_FORMAT.format(DATE);
+           System.out.println(date);*/
+
+           String string = StrInArray[6];
+           DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy", Locale.ENGLISH);
+           LocalDate date = LocalDate.parse(string, formatter);
+           System.out.println(date); // 2010-01-02
+
+
+
+           //NewCred.termOfFirstPayment = date;
+
+           prn(NewCred.percent);
+           prn(NewCred.termOfFirstPayment);
+           //System.out.println(Day);
        }    
 
+       public static void prn(int nc) //��� ������ �� �����
+       {
+            System.out.println(nc);
+
+       }
+       public static void prn(Date nc) //��� ������ �� �����
+       {
+            System.out.println(nc);
+
+       }
     }
