8d22af5c1388c5a18334bbc405fbeafba366e849
Chimba <chimbeevaa@gmail.com>
2020-02-10 08:03:23 +0800

    tomcat server

---
+++ CredCalcTomcatServer/src/java/CredCalcTomcatServer/Controller.java
@@ -0,0 +1,48 @@
+package CredCalcTomcatServer;
+
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
+
+
+//---------------------------Controller Begin ------------------------------
+
+public class Controller //класс контроллер - приём запроса от пользователя;анализ запроса; выбор следующего действия системы, соответственно результатам анализа 
+//(например, передача запроса другим элементам системы).
+//Отправляем на HTTPServer get запрос с параметрами кредита и получаем результаты расчета кредита
+
+{
+    int id_client;
+    int size;
+    int percent;
+    int first_pay_size;
+    char typeOfPayment;
+    int term;
+    String termOfFirstPayment;
+
+    
+    public static void main(String[] args) throws IOException, ParseException, Exception
+       {
+        
+            List<String> dataListIn = View.ReadCrd2(); //производим чтение из файла данных по кредитам и получаем данные в виде list
+
+            //берем 10 кредитов и отправляем их на сервер для расчета 
+            String credStr = "";
+            //HTTPServer3.main(args); //запускаем http сервер
+            for (int i = 10;i<21;i++)
+            {   
+                credStr = dataListIn.get(i);
+                View.GetCrdFromSrv(credStr); //отправляем на сервер строку с параметрами кредита
+            }
+        
+       }       
+           
+        
+}    
+
+   
+
+//-------------------------------------Controller End------------------------------------------------------------
+
+
---
+++ CredCalcTomcatServer/src/java/CredCalcTomcatServer/HTTPServer3.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package CredCalcTomcatServer;
+
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author 1
+ */
+public class HTTPServer3 
+{
+  public static void main(String[] args) throws IOException {
+      HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+      HttpContext context = server.createContext("/");
+      context.setHandler(HTTPServer3::handleRequest);
+      server.start();
+ 
+  }
+
+  private static void handleRequest(HttpExchange exchange) throws IOException {
+      String response = "Ответ сервера:";
+      exchange.sendResponseHeaders(200, response.getBytes().length);//response code and length
+      InputStream is=exchange.getRequestBody();
+      OutputStream os = exchange.getResponseBody();
+      os.write(response.getBytes());
+      os.close();
+      String respo = exchange.getRequestURI().getQuery();
+      Map<String, String> params = queryToMap(exchange.getRequestURI().getQuery()); 
+      System.out.println("Сервер : поступили данные по кредиту " + respo);
+      List<String> strCredOutlist = new ArrayList<>();
+      List<String> strCredlist = new ArrayList<>();
+      strCredOutlist.add(respo);
+      
+      strCredlist = Model.getCrd(strCredOutlist);//передаем для расчета кредитов
+      System.out.println("Сервер : Результаты расчета:");
+      View.ShowList(strCredlist);
+      View.WriteJson(strCredlist, respo.substring(0, 2));
+      System.out.println("Сервер : Результаты расчета записаны в файл json");
+   
+  }
+  
+  public static Map<String, String> queryToMap(String query) 
+      {
+        Map<String, String> result = new HashMap<>();
+        for (String param : query.split("&")) 
+        {
+            String[] entry = param.split("=");
+            if (entry.length > 1) {
+                result.put(entry[0], entry[1]);
+            }else{
+                result.put(entry[0], "");
+            }
+        }
+        return result;
+      }
+}   
+
