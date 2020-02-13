e1c03f6f3666cf7e434b44affd4891432e97a289
Chimba <chimbeevaa@gmail.com>
2020-02-13 20:06:44 +0800

    ee

--- CredCalcTomcatServer/src/java/NewServlet.java
+++ CredCalcTomcatServer/src/java/NewServlet.java
@@ -4,12 +4,25 @@
  * and open the template in the editor.
  */
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import static org.eclipse.jdt.internal.compiler.parser.Parser.name;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+
 
 /**
  *
@@ -60,23 +73,57 @@ public class NewServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
     throws IOException, ServletException
     {
-        response.setContentType("text/html;charset=UTF-8");
+        //read input
+        response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+        out.println("Method: " + request.getMethod());
+        out.println("Input data: " + request.getParameter("Param1"));
+        System.out.println("Сервер : поступили данные по кредиту " + request.getParameter("Param1"));
+        List<String> strCredOutlist = new ArrayList<>();
+        List<String> strCredlist = new ArrayList<>();
+        strCredOutlist.add(respo);
+
+        strCredlist = Model.getCrd(strCredOutlist);//передаем для расчета кредитов
+        System.out.println("Сервер : Результаты расчета:");
+        View.ShowList(strCredlist);
+        View.WriteJson(strCredlist, respo.substring(0, 2));
+        System.out.println("Сервер : Результаты расчета записаны в файл json");
+        /*
+        response.setContentType("application/json; utf-8");
        
-        //out.println("<html>");
-        //out.println("<body>");
-        //out.println("<head>");
-        //out.println("<title>Request Information Example</title>");
-        //out.println("</head>");
-        //out.println("<body>");
-        //out.println("<h3>Request Information Example</h3>");
-        //out.println("Method: " + request.getMethod());
-        //out.println("Request URI: " + request.getRequestURI());
-        //out.println("Protocol: " + request.getProtocol());
-        //out.println("PathInfo: " + request.getPathInfo());
-        //out.println("Remote Address: " + request.getRemoteAddr());
-        out.println("Params " + request.getParameter("Param1")); 
+        JSONObject json = new JSONObject();
+        json.put("name", "student");
+
+        JSONArray array = new JSONArray();
+        JSONObject item = new JSONObject();
+        item.put("information", "test");
+        item.put("id", 3);
+        item.put("name", "course1");
+        array.add(item);
+
+        json.put("course", array);
+        //OutputStream os = new OutputStream();
+        String jsonInputString = json.toString();
         
+        try(OutputStream os = response.getOutputStream()) 
+        {
+            byte[] input = jsonInputString.getBytes("utf-8");
+            os.write(input, 0, input.length);           
+        }
+        /*read response
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) 
+        {
+            StringBuilder response1 = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) 
+            {
+                response1.append(responseLine.trim());
+            }
+            System.out.println(response1.toString());
+        }
+        
+        
+        */
         //out.println("</body>");
         //out.println("</html>");
     }
