ff7505dd3b3cf02b99e8e29a0eef7e80dab8acd6
Chimba <chimbeevaa@gmail.com>
2020-02-17 21:25:40 +0800

    l

---
+++ demo/src/main/java/com/example/demo/RepresentClass.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+/**
+ *
+ * @author тест
+ */
+public class RepresentClass {
+    
+	private long id = 0;
+        
+	private String content = "";
+
+	public RepresentClass(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
+
